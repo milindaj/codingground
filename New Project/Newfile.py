ee9035d539d33e673df54c681102307a41e93585
milindaj <milindaj@gmail.com>
2015-01-22 07:59:19 +0530

    Update Newfile.py

--- New Project/Newfile.py
+++ New Project/Newfile.py
@@ -1,44 +1,26 @@
-
-
-def toggleBit(int_type, offset):
-  mask = 1 << offset
-  return(int_type ^ mask)
-
 N = input()
 K = input()
 S = str(input())
 assert 1 <= N <= 10**6
 assert 1 <= K <= 10**6
 
-
-
 #print(bool(S))
 lenT = len(S)
 
-#arr=bytearray(S)
 
 b = S[lenT-1] #-48
 newS = str(b)
 
 b = int(b) ^ int(S[lenT-2]) #-48)
 newS = str(b) + newS
-x = 0
+
 for i in range(lenT-3, N-K-1, -1):
     lenS = len(newS)
-    if lenS<(K-1):
-        bb = int(newS[lenS-1])
-        for j in range(lenS-2, -1, -1):
-            bb = bb^int(newS[j])
-        b = int(S[i])^int(bb)
-        newS = str(b) + newS
-    elif lenS < N:
-
-        bb = int(newS[lenS-1-x])
-        for j in range(lenS-2-x, (lenS-1-x-K), -1):
-            bb = bb^int(newS[j])
-        b = int(S[i])^int(bb)
-        newS = str(b) + newS
-        x = x + 1
+    bb = newS[lenS-1]
+    for j in range(lenS-2, -1, -1):
+        bb = int(bb)^int(newS[j])
+    b = int(S[i])^int(bb)
+    newS = str(b) + newS
 
 print(newS)
 
